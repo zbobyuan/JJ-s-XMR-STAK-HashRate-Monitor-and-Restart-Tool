e32af159cd2a0ccf8b95817fe8518116df289e66
mutl3y <mark@heynesit.co.uk>
2018-05-04 23:22:19 +0100

    Delete Run_HashMonitor.cmd

--- Run_HashMonitor.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~DP0JJs_HashMonitor.ps1""' -Verb RunAs}"
